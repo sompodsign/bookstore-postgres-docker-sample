914799bdffb162228278c3e49db2851f48a539d6
sompodsign <sompodsign@gmail.com>
2022-01-01 08:44:07 +0600

    till homepage

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #local
     'users.apps.UsersConfig',
+    'pages.apps.PagesConfig'
 ]
 AUTH_USER_MODEL = 'users.CustomUser'
 
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'bookstore_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
