84d39ae8495910ed814be57f180da56c43229320
sompodsign <sompodsign@gmail.com>
2022-01-03 07:41:38 +0600

    tests added for users app

--- users/tests.py
+++ users/tests.py
@@ -1,10 +1,19 @@
+from django.urls import resolve
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from .forms import CustomUserCreationForm
+from .views import SignupPageView
 
 
 # Create your tests here.
 
 class CustomUserTests(TestCase):
+
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
 
     def test_create_user(self):
         User = get_user_model()
@@ -31,3 +40,23 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+    def test_sign_up_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(
+            self.response,
+            'Hi there! I should not be on the page.'
+        )
+
+    def test_sign_up_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+
+    def test_signup_view(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(
+            view.func.__name__,
+            SignupPageView.as_view().__name__
+        )
