98c32b136356d1d17eb559e6680ff5f9fdbe5058
sompodsign <sompodsign@gmail.com>
2022-01-04 07:34:02 +0600

    tests added for about page

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -38,11 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #local
+    # Third-party
+    'crispy_forms',
+
+    # local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig'
 ]
 AUTH_USER_MODEL = 'users.CustomUser'
+
+# django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -124,7 +130,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- pages/tests.py
+++ pages/tests.py
@@ -35,3 +35,22 @@ class HomepageTests(SimpleTestCase):
             view.func.__name__,
             HomePageView.as_view().__name__
         )
+
+
+class AboutPageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_about_page(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_about_page_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+    def test_about_contains_correct_html(self):
+        self.assertContains(self.response, 'About Page')
+
+    def test_about_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
