a621b51148c2eb072e706385ff1358ce30a5a882
sompodsign <sompodsign@gmail.com>
2022-01-07 07:14:58 +0600

    users test for allauth

--- users/tests.py
+++ users/tests.py
@@ -1,49 +1,23 @@
-from django.urls import resolve
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-
-from .forms import CustomUserCreationForm
-from .views import SignupPageView
 
 
 # Create your tests here.
 
 class CustomUserTests(TestCase):
 
+    username = 'newuser'
+    email = 'newuser@email.com'
+
     def setUp(self):
-        url = reverse('signup')
+        url = reverse('account_signup')
         self.response = self.client.get(url)
-
-    def test_create_user(self):
-        User = get_user_model()
-        user = User.objects.create_user(
-            username='will',
-            email='will@gmail.com',
-            password='testpass123'
-        )
-        self.assertEqual(user.username, 'will')
-        self.assertEqual(user.email, 'will@gmail.com')
-        self.assertTrue(user.is_active)
-        self.assertFalse(user.is_staff)
-        self.assertFalse(user.is_superuser)
-
-    def test_create_superuser(self):
-        User = get_user_model()
-        admin_user = User.objects.create_superuser(
-            username='superadmin',
-            email='superadminemail@gmail.com',
-            password='testpass123'
-        )
-        self.assertEqual(admin_user.username, 'superadmin')
-        self.assertEqual(admin_user.email, 'superadminemail@gmail.com')
-        self.assertTrue(admin_user.is_active)
-        self.assertTrue(admin_user.is_staff)
-        self.assertTrue(admin_user.is_superuser)
 
     def test_sign_up_template(self):
         self.assertEqual(self.response.status_code, 200)
-        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertTemplateUsed(self.response, 'accounts_signup.html')
         self.assertContains(self.response, 'Sign Up')
         self.assertNotContains(
             self.response,
@@ -51,12 +25,8 @@ class CustomUserTests(TestCase):
         )
 
     def test_sign_up_form(self):
-        form = self.response.context.get('form')
-        self.assertIsInstance(form, CustomUserCreationForm)
-
-    def test_signup_view(self):
-        view = resolve('/accounts/signup/')
-        self.assertEqual(
-            view.func.__name__,
-            SignupPageView.as_view().__name__
-        )
+        new_user = get_user_model().objects.create_user(
+            self.username, self.email)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
