f015fab10347edfa974e07ca4c00a1ef314af964
sompodsign <sompodsign@gmail.com>
2022-01-11 11:29:34 +0600

    reviews, media

--- books/admin.py
+++ books/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Review
 
 
 # Register your models here.
 # admin.site.register(Book)
 
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+    inlines = [
+        ReviewInline,
+    ]
     list_display = ('title', 'author', 'price')
