feb2e00df1ee8e79fa13c96d09f30b064ea2651c
sompodsign <sompodsign@gmail.com>
2022-01-12 11:58:56 +0600

    search feature

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, \
     PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 from .models import Book
 
 
@@ -22,3 +23,14 @@ class BookDetailView(LoginRequiredMixin,
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self):
+        return Book.objects.filter(
+            Q(title__icontains='morning') | Q(title__icontains='javascript')
+        )
